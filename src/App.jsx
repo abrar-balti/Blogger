57e057cda30aad12c3b5761547989d4cd37f79a2
abrarbalti <abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.com>
2025-06-05 13:03:20 +0500

    add header footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authServices from "./appwrite/auth";
+import { login, logout } from "./features/auth/authSlice";
+import { Header, Footer } from "./components/index";
+import Outlet from "react-dom";
 function App() {
-  return (
-    <>
-      <h1 className="text-white">bloger</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authServices
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
