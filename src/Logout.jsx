cb9a014136e8373a8ab2cdad7a2bcfc44cae7753
abrarbalti <abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.comgit config --global user.email abrarbulti@gmail.com>
2025-06-16 17:40:45 +0500

    add button components

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -4,6 +4,12 @@ import authServices from "./appwrite/auth";
 import { logout } from "./features/auth/authSlice";
 
 const Logout = () => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authServices.logout().then(() => {
+      dispatch(logout);
+    });
+  };
   return (
     <button
       className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,7 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Container from "../components/container/Container";
-export { Header, Footer, Logo, Container };
+import Logout from "../Logout";
+import Button from "./Button";
+import InputBox from "./InputBox";
+export { Header, Footer, Logo, Container, Logout, Button, InputBox };
